ebbb2d50e70d6173978001a99d6a2c3672a13770
Nickolas Burr <nickolasburr@gmail.com>
2019-07-18 14:36:57 -0400

    Add Settings system model

---
+++ Model/SystemModel/Module/Settings.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Settings.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License, which
+ * is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/stackdriverlogger/LICENSE.txt
+ *
+ * @package       AuroraExtensions_StackdriverLogger
+ * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
+ * @license       MIT License
+ */
+declare(strict_types=1);
+
+namespace AuroraExtensions\StackdriverLogger\Model\SystemModel\Module;
+
+use Magento\Framework\{
+    DataObject,
+    DataObjectFactory
+};
+
+class Settings
+{
+    /** @property DataObject $container */
+    protected $container;
+
+    /**
+     * @param DataObjectFactory $dataObjectFactory
+     * @param array $data
+     * @return void
+     */
+    public function __construct(
+        DataObjectFactory $dataObjectFactory,
+        array $data = []
+    ) {
+        $this->container = $dataObjectFactory->create($data);
+    }
+
+    /**
+     * @return DataObject|null
+     */
+    public function getContainer(): ?DataObject
+    {
+        return $this->container;
+    }
+
+    /**
+     * @return array
+     */
+    public function getDataTypes(): array
+    {
+        return $this->getContainer()->getData('data_types') ?? [];
+    }
+}
