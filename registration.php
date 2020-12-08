14c6aec7e7500f9d6fb65a31d3c7eae644303b90
Nickolas Burr <nickolasburr@gmail.com>
2020-12-07 20:08:28 -0500

    Update source file license comment

--- registration.php
+++ registration.php
@@ -4,15 +4,15 @@
  *
  * NOTICE OF LICENSE
  *
- * This source file is subject to the MIT License, which
+ * This source file is subject to the MIT license, which
  * is bundled with this package in the file LICENSE.txt.
  *
  * It is also available on the Internet at the following URL:
  * https://docs.auroraextensions.com/magento/extensions/2.x/stackdriver/LICENSE.txt
  *
- * @package       AuroraExtensions_Stackdriver
+ * @package       AuroraExtensions\Stackdriver
  * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
- * @license       MIT License
+ * @license       MIT
  */
 use Magento\Framework\Component\ComponentRegistrar;
 
