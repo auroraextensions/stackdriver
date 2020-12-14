0e204fc8ae2f62d87bd53b9b106df8911001cdc0
Nickolas Burr <nickolasburr@gmail.com>
2020-12-14 01:19:16 -0500

    Create ReportedErrorEventMetadataProviderInterface

---
+++ Api/ReportedErrorEventMetadataProviderInterface.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * ReportedErrorEventMetadataProviderInterface.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT license, which
+ * is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/stackdriver/LICENSE.txt
+ *
+ * @package       AuroraExtensions\Stackdriver\Api
+ * @copyright     Copyright (C) 2020 Aurora Extensions <support@auroraextensions.com>
+ * @license       MIT
+ */
+declare(strict_types=1);
+
+namespace AuroraExtensions\Stackdriver\Api;
+
+interface ReportedErrorEventMetadataProviderInterface
+{
+    /**
+     * @return string
+     */
+    public function getTypeUrn(): string;
+}
