35cf5c41fc127fbe808eefab05a41f6947756bab
Nickolas Burr <nickolasburr@gmail.com>
2020-12-14 01:18:41 -0500

    Create InvalidStackdriverSetupException class

---
+++ Exception/InvalidStackdriverSetupException.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * InvalidStackdriverSetupException.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT license, which
+ * is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/stackdriver/LICENSE.txt
+ *
+ * @package       AuroraExtensions\Stackdriver\Exception
+ * @copyright     Copyright (C) 2020 Aurora Extensions <support@auroraextensions.com>
+ * @license       MIT
+ */
+declare(strict_types=1);
+
+namespace AuroraExtensions\Stackdriver\Exception;
+
+use Magento\Framework\Exception\LocalizedException;
+
+class InvalidStackdriverSetupException extends LocalizedException
+{
+}
